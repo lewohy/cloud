748dca6690714b3f614ae6cf60368f52ec494854
lewohy <lwh8762@gmail.com>
2022-11-07 02:48:13 +0900

    feat: 스크롤바를 비율에 따른 크기조절과 스크롤에 따른 위치조절 완료

--- public/components/ScrollView/index.tsx
+++ public/components/ScrollView/index.tsx
@@ -1,12 +1,58 @@
+import { useTheme } from '@suid/material';
 import Box from '@suid/material/Box';
-import { JSX } from 'solid-js';
+import { createEffect, createMemo, createSignal, JSX } from 'solid-js';
 
 export interface ScrollViewProps {
     children: JSX.Element;
+    /**
+     * @default 10
+     */
+    width?: number;
+    /**
+     * @default 10
+     */
+    minThumbHeightRatio?: number;
+
 }
 
 export const ScrollView = (props: ScrollViewProps) => {
-    
+    const theme = useTheme();
+
+    const [view, setView] = createSignal<HTMLDivElement | null>(null);
+    const [content, setContent] = createSignal<HTMLDivElement | null>(null);
+
+    const [viewHeight, setViewHeight] = createSignal(0);
+    const [contentHeight, setContentHeight] = createSignal(0);
+    const [padding, setPadding] = createSignal(5);
+    const [scroll, setScroll] = createSignal(0);
+    const width = createMemo(() => props.width ?? 10);
+    const minThumbHeightRatio = createMemo(() => props.minThumbHeightRatio ?? 10);
+
+    const scrollBarHeight = createMemo(() => viewHeight() - padding() * 2);
+    const thumbHeight = createMemo(() => viewHeight() / contentHeight() * scrollBarHeight());
+
+    createEffect(() => {
+        console.log(view());
+        console.log(content());
+        console.log(viewHeight(), contentHeight());
+
+        if (view() !== null) {
+            new ResizeObserver(e => {
+                e.forEach(entry => {
+                    setViewHeight(entry.target.clientHeight);
+                });
+            }).observe(view()!);
+        }
+
+        if (content() !== null) {
+            new ResizeObserver(e => {
+                e.forEach(entry => {
+                    setContentHeight(entry.target.clientHeight);
+                });
+            }).observe(content()!);
+        }
+    });
+
     return (
         <Box
             sx={{
@@ -16,11 +62,49 @@ export const ScrollView = (props: ScrollViewProps) => {
                 '&::-webkit-scrollbar': {
                     display: 'none'
                 }
-            }}>
-            {
-                props.children
-            }
-            
-        </Box>
+            }}
+            onScroll={e => {
+                console.log('s', scroll());
+                console.log('c', contentHeight());
+                console.log('b', scrollBarHeight());
+                setScroll(view()?.scrollTop ?? 0);
+            }}
+            ref={setView}>
+            <Box
+                ref={setContent}>
+                {
+                    props.children
+                }
+            </Box>
+
+            <Box
+                sx={{
+                    width: 'auto',
+                    height: '100%',
+                    padding: `${padding()}px`,
+                    position: 'absolute',
+                    right: '0',
+                    top: '0'
+                }}>
+                <Box
+                    sx={{
+                        width: width(),
+                        height: scrollBarHeight(),
+                        background: 'rgba(0, 0, 0, 0.1)',
+                        borderRadius: width() / 2,
+                        overflow: 'hidden'
+                    }}>
+                    <Box
+                        sx={{
+                            width: width(),
+                            height: `${thumbHeight()}px`,
+                            background: theme.palette.primary.main,
+                            borderRadius: width() / 2,
+                            marginTop: `${scroll() / contentHeight() * scrollBarHeight()}px`,
+                            transition: 'height 0.2s linear, margin-top 0.1s linear'
+                        }} />
+                </Box>
+            </Box>
+        </Box >
     );
 };
