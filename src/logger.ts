1754313945ae57807507b148985a899eb402971c
lewohy <lwh8762@gmail.com>
2022-11-08 23:51:13 +0900

    fix: 요청 실패시 응답코드 변경

--- src/logger.ts
+++ src/logger.ts
@@ -5,23 +5,25 @@ const logger = winston.createLogger({
     level: 'info',
     transports: [
         new winston.transports.Console({
-            format: winston.format.combine(winston.format.colorize(), winston.format.simple(), winston.format.errors({ stack: true })),
+            format: winston.format.combine(winston.format.colorize(), winston.format.simple(), winston.format.errors({
+                stack: true
+            })),
         })
     ]
 });
 
 // REVIEW: 함수 위치 바꿔야할듯
 export function sendError<T extends cloud.protocol.storage.Response>(res: core.Response<T>, e: unknown): void {
-    logger.error(e);
+    logger.error((e as Error).message);
 
     if (e instanceof Error) {
-        res.status(500).send({
+        res.status(202).send({
             error: {
                 message: e.message,
             }
         } as T);
     } else {
-        res.status(500).send({
+        res.status(202).send({
             error: {
                 message: 'unknown error'
             }
