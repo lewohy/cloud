5271148e4c0fcc10880940df5e5e8f9c0306142f
lewohy <lwh8762@gmail.com>
2022-11-14 00:42:48 +0900

    fix: ScrollView 구조 변경

--- public/components/ScrollView/index.tsx
+++ public/components/ScrollView/index.tsx
@@ -1,17 +1,18 @@
 import { useTheme } from '@suid/material';
 import Box from '@suid/material/Box';
+import Stack from '@suid/material/Stack';
 import { createEffect, createMemo, createSignal, JSX } from 'solid-js';
 
 export interface ScrollViewProps {
     children: JSX.Element;
     /**
-     * @default 7
+     * @default 8
      */
     width?: number;
     /**
-     * @default 8
+     * @default 4
      */
-    minThumbHeightRatio?: number;
+    padding?: number;
 
 }
 
@@ -21,85 +22,53 @@ export const ScrollView = (props: ScrollViewProps) => {
 
     const [view, setView] = createSignal<HTMLDivElement | null>(null);
     const [content, setContent] = createSignal<HTMLDivElement | null>(null);
-    const [thumb, setThumb] = createSignal<HTMLDivElement | null>(null);
 
-    const [viewHeight, setViewHeight] = createSignal(0);
-    const [contentHeight, setContentHeight] = createSignal(0);
-    const [padding, setPadding] = createSignal(5);
-    const [scroll, setScroll] = createSignal(0);
-    const width = createMemo(() => props.width ?? 7);
-
-    const scrollBarHeight = createMemo(() => viewHeight() - padding() * 2);
-    const thumbHeight = createMemo(() => Math.min(scrollBarHeight(), viewHeight() / contentHeight() * scrollBarHeight()));
-
-    createEffect(() => {
-        if (view() !== null) {
-            new ResizeObserver(e => {
-                e.forEach(entry => {
-                    setViewHeight(entry.target.clientHeight);
-                });
-            }).observe(view()!);
-        }
-
-        if (content() !== null) {
-            new ResizeObserver(e => {
-                e.forEach(entry => {
-                    setContentHeight(entry.target.clientHeight);
-                });
-            }).observe(content()!);
-        }
-    });
+    const padding = createMemo(() => props.padding ?? 4);
+    const width = createMemo(() => props.width ?? 8);
 
     return (
-        <Box
+        <Stack
             ref={setView}
             sx={{
                 width: '100%',
                 height: '100%',
                 overflowY: 'scroll',
                 '&::-webkit-scrollbar': {
-                    display: 'none'
+                    width: `auto`,
+                    overflow: 'visible'
+                },
+                '&::-webkit-scrollbar-button': {
+                    width: '0px',
+                    height: '0px',
+                },
+                '&::-webkit-scrollbar-thumb': {
+                    width: `${width()}px`,
+                    background: theme.palette.primary.light,
+                    backgroundClip: 'padding-box',
+                    borderRadius: `${width()}px`,
+                    border: `${padding()}px solid transparent`,
+                    boxSizing: 'content-box'
+                },
+                '&::-webkit-scrollbar-thumb:active': {
+                    width: `${width()}px`,
+                    background: theme.palette.primary.dark,
+                    backgroundClip: 'padding-box',
+                    borderRadius: `${width()}px`,
+                    border: `${padding()}px solid transparent`,
+                    boxSizing: 'content-box'
+                },
+                '&::-webkit-scrollbar-track': {
+                    background: 'transparent',
+                    borderRadius: `${width() / 2}px`,
+                    margin: `${padding()}px`,
                 }
-            }}
-            onScroll={e => {
-                setScroll(view()?.scrollTop ?? 0);
             }}>
-            <Box
+            <Stack
                 ref={setContent}>
                 {
                     props.children
                 }
-            </Box>
-
-            <Box
-                sx={{
-                    width: 'auto',
-                    height: '100%',
-                    padding: `${padding()}px`,
-                    position: 'absolute',
-                    right: '0',
-                    top: '0'
-                }}>
-                <Box
-                    sx={{
-                        width: `${width()}px`,
-                        height: `${scrollBarHeight()}px`,
-                        background: theme.palette.primary.light,
-                        borderRadius: `${width() / 2}px`,
-                        overflow: 'hidden'
-                    }}>
-                    <Box
-                        ref={setThumb}
-                        sx={{
-                            width: `${width()}px`,
-                            height: `${thumbHeight()}px`,
-                            background: theme.palette.primary.dark,
-                            borderRadius: `${width() / 2}px`,
-                            marginTop: `${scroll() / contentHeight() * scrollBarHeight()}px`,
-                            transition: 'height 0.2s ease-in-out'
-                        }} />
-                </Box>
-            </Box>
-        </Box >
+            </Stack>
+        </Stack >
     );
 };
