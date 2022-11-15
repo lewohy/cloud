50aaec1fb8b854be4033263f58e11b242d4e80fc
lewohy <lwh8762@gmail.com>
2022-11-15 16:16:41 +0900

    chore: logger 수정

--- src/logger.ts
+++ src/logger.ts
@@ -1,29 +1,60 @@
 import winston from 'winston';
 import * as core from 'express-serve-static-core';
+import config from '~/config.json';
+import { isError } from './typguard';
 
 const logger = winston.createLogger({
     level: 'info',
     transports: [
-        new winston.transports.Console({
-            format: winston.format.combine(winston.format.colorize(), winston.format.simple(), winston.format.errors({
-                stack: true
-            })),
+        new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        winston.format.printf(info => {
+            return `[${info.timestamp}] [${info.level}]: ${info.message}`;
+        }),
+        winston.format.colorize({
+            all: true
+        }),
+    )
+});
+
+const debugLogger = winston.createLogger({
+    level: 'debug',
+    transports: [
+        new winston.transports.File({
+            filename: config.log.debug
         })
-    ]
+    ],
+    format: winston.format.combine(
+        winston.format.errors({
+            stack: true
+        }),
+        winston.format.timestamp(),
+        winston.format.json(),
+        winston.format.prettyPrint(),
+    )
 });
 
 // REVIEW: 함수 위치 바꿔야할듯
 export function sendError<T extends cloud.protocol.storage.Response>(res: core.Response<T>, e: unknown): void {
-    logger.error((e as Error).message);
+    if (isError(e)) {
+        logger.error(e.message);
+        debugLogger.error(e);
 
-    if (e instanceof Error) {
-        res.status(202).send({
+        res.status(400).send({
             error: {
                 message: e.message,
             }
         } as T);
     } else {
-        res.status(202).send({
+        const error = new Error(`Unknown error.\n${JSON.stringify(e, null, 4)}`);
+        logger.error(error.message);
+        debugLogger.error(error);
+
+        res.status(400).send({
             error: {
                 message: 'unknown error'
             }
