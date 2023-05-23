77ed0766c944da5b4602862abe7eacfcd60e5139
lewohy <lwh8762@gmail.com>
2023-05-23 11:24:24 +0900

    fix: 로거 수정

--- src/logger.ts
+++ src/logger.ts
@@ -9,19 +9,21 @@ const logFilePath = path.resolve(config.log.base, dayjs().format(config.log.debu
 
 const logger = {
     info: (message: string) => {
-        const stack = new Error().stack?.split('\n').slice(1).map((e) => e.trim());
+        const stack = new Error().stack?.split('\n').slice(2).map((e) => e.trim());
 
         winstonLogger.info(message, {
             stack
         });
     },
-    error: (e: unknown) => {
-        const stack = new Error().stack?.split('\n').slice(1).map((e) => e.trim());
+    error: (e: any) => {
+        const stack = new Error().stack?.split('\n').slice(2).map((e) => e.trim());
 
-        winstonLogger.error(e);
+        winstonLogger.error(e, {
+            stack
+        });
     },
     warn: (message: string) => {
-        const stack = new Error().stack?.split('\n').slice(1).map((e) => e.trim());
+        const stack = new Error().stack?.split('\n').slice(2).map((e) => e.trim());
 
         winstonLogger.warn(message, {
             stack
@@ -46,7 +48,7 @@ const winstonLogger = winston.createLogger({
             filename: logFilePath,
             format: winston.format.combine(
                 winston.format.printf(info => {
-                    return `[${info.timestamp}] [${info.level}]: ${info.message}` + (info.stack ? info.stack.map((e: string) => `\t${e}`).join('\n') : '');
+                    return `[${info.timestamp}] [${info.level}]: ${info.message}` + (info.stack ? '\n' + info.stack.map((e: string) => `\t${e}`).join('\n') : '');
                 })
             )
         }),
